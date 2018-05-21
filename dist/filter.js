dda2c441c6e6a0d37a11cf45acf69d29325595c3
Ronny Elflein <Ronny@11lein.de>
2018-05-21 22:04:57 +0200

    outsourced Text

--- dist/filter.js
+++ dist/filter.js
@@ -2,47 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const instaMock_1 = require("./instaMock");
 const wordpressMock_1 = require("./wordpressMock");
+const textMock_1 = require("./textMock");
 // init Mocks
 let Instagram = new instaMock_1.InstagramMock;
 let Wordpress = new wordpressMock_1.WordpressMock;
-Instagram.newPhotoByYouTagged.Caption = `Vanillepuddingkuchen mit Schokoboden und Erdbeeren 🍓😍
-Wer möchte?
-Danke an alle die bereits an der #diespicebarrastetaus Challenge mitgemacht haben! Wer noch dabei sein möchte, bis Freitag 12 Uhr habt ihr die Chance auf 3x3 Gewürze von der @spice_bar 💚. Alle Infos findet ihr über mein Profil!💚 [Werbung]
-.
-Ich bin aus dem Bett gefallen und steh schon in den Schuhen. Irgendwie muss ich mich hier immer zwischen Straßenlärm und Ersticken entscheiden. 🤐
-Naja, der frühe Vogel und so... blah blah... Für mich geht es heute in die Museen Berlins. Mal sehen wie viel ich schaffe. 😂 Entweder renne ich durch sowas durch, oder ich verliere mich und vergesse Raum und Zeit...
-Welches ist denn euer MUST-SEE Museum in Berlin?😀
-.
-.
-[Rezept enthält Werbung]
-➡️Für den Boden 25g Kokosmehl, 25g Dinkelgrieß, 10g Backkakao, 1 Ei, Süße und Tonkabohne @spice_bar mit ca 60ml Milch verrühren bis ein formbarer Teig entsteht, in eine 12er Form füllen und an den Rändern etwas hochziehen, ca 20 Minuten bei 180°Grad backen
-➡️Vanillepudding mit 350ml Milch und 2EL Erythrit @spice_bar kochen (oder eine andere Süße) und in den Boden füllen
-➡️Erdbeeren rein und abkühlen lassen.
-🍓😍
-.
-____________________
-Auf Spicebar.de könnt ihr ab 15€ Einkauf mit spicytina 5€ sparen 💚
-🍫🍫🍫🍫🍫
-#foodblog #puddingkuchen #esstmehrkuchen #kuchengehtimmer #healthycake #projektgesundleben #gesundbacken #backenistliebe #breakfastcake #frühstückskuchen #frühstücksliebe #lowcarbkuchen #lowcarbfrühstück #bragyourplate #kokosmehl #sweettooth #fitfamgermany #foodblogger_de #foodlover #himbeeren #vanillepudding #erdbeeren #erdbeerkuchen #strawberry #strawberrycake #heresmyfood #cakelover #cakeporn`;
-Instagram.newPhotoByYouTagged.CaptionNoTag = `Vanillepuddingkuchen mit Schokoboden und Erdbeeren 🍓😍
-Wer möchte?
-Danke an alle die bereits an der #diespicebarrastetaus Challenge mitgemacht haben! Wer noch dabei sein möchte, bis Freitag 12 Uhr habt ihr die Chance auf 3x3 Gewürze von der @spice_bar 💚. Alle Infos findet ihr über mein Profil!💚 [Werbung]
-.
-Ich bin aus dem Bett gefallen und steh schon in den Schuhen. Irgendwie muss ich mich hier immer zwischen Straßenlärm und Ersticken entscheiden. 🤐
-Naja, der frühe Vogel und so... blah blah... Für mich geht es heute in die Museen Berlins. Mal sehen wie viel ich schaffe. 😂 Entweder renne ich durch sowas durch, oder ich verliere mich und vergesse Raum und Zeit...
-Welches ist denn euer MUST-SEE Museum in Berlin?😀
-.
-.
-[Rezept enthält Werbung]
-➡️Für den Boden 25g Kokosmehl, 25g Dinkelgrieß, 10g Backkakao, 1 Ei, Süße und Tonkabohne @spice_bar mit ca 60ml Milch verrühren bis ein formbarer Teig entsteht, in eine 12er Form füllen und an den Rändern etwas hochziehen, ca 20 Minuten bei 180°Grad backen
-➡️Vanillepudding mit 350ml Milch und 2EL Erythrit @spice_bar kochen (oder eine andere Süße) und in den Boden füllen
-➡️Erdbeeren rein und abkühlen lassen.
-🍓😍
-.
-____________________
-Auf Spicebar.de könnt ihr ab 15€ Einkauf mit spicytina 5€ sparen 💚
-🍫🍫🍫🍫🍫
-#puddingkuchen #esstmehrkuchen #kuchengehtimmer #healthycake #projektgesundleben #gesundbacken #backenistliebe #breakfastcake #frühstückskuchen #frühstücksliebe #lowcarbkuchen #lowcarbfrühstück #bragyourplate #kokosmehl #sweettooth #fitfamgermany #foodblogger_de #foodlover #himbeeren #vanillepudding #erdbeeren #erdbeerkuchen #strawberry #strawberrycake #heresmyfood #cakelover #cakeporn`;
+let textMock = new textMock_1.TextMock();
+Instagram.newPhotoByYouTagged.Caption = textMock.post[0];
+Instagram.newPhotoByYouTagged.CaptionNoTag = textMock.post[1];
 class Filter {
     run() {
         /** Snip start filter code */
@@ -51,7 +17,6 @@ class Filter {
         let arr2 = Instagram.newPhotoByYouTagged.CaptionNoTag.split(" ");
         // get the hashtag by diff the CaptionNoTag
         let hashTag = arr1.filter(x => arr2.indexOf(x) < 0)[0];
-        console.log(hashTag);
         // get the title  (caption from start to first linebreak)
         let title = caption.substring(0, caption.indexOf("\n"));
         // get the text body after first line break to the hashtag
---
+++ src/textMock.ts
@@ -0,0 +1,123 @@
+export class TextMock{
+    post:string[]= [`Vibrant avocado vegan salad recipe 🥑🥑🥑😍😍😍😍.
+    .
+    .
+    Ingredients:
+    
+    1 1/2 cups uncooked #quinoa
+    
+    3 cups water
+    
+    2 #carrots
+    
+    1/4 white cabbage (approx. 1 cup)
+    
+    4 kale leaves
+    
+    1 broccoli head
+    
+    2 beetroot, cooked
+    
+    1 package (200g) firm tofu
+    
+    1/3 cup pomegranate seeds
+    
+    1 bunch chives
+    
+    1/2 cup packed mint leaves
+    
+    1/2 cup packed #cilantro
+    
+    1 avocado
+    
+    salt and pepper to taste
+    
+    For the dressing:
+    
+    the juice of 2 lemons
+    
+    3-4 garlic cloves, diced
+    
+    1/3 cup tahini
+    
+    1/4 cup tamari, or soy sauce
+    
+    2 tbsp apple cider vinegar,  or more lemon juice
+    
+    1 tbsp olive oil
+    
+    pinch #cumin
+    
+    pinch ground ginger 
+    Directions:
+    
+    Begin by cooking your quinoa. Place 1.5 cups quinoa and 3 cups water in a saucepan. Bring to a boil then reduce heat and let simmer for 12-15 minutes, or until cooked. Remove from stovetop and place in a large mixing bowl to cool.
+    
+    Peel and grate your carrots. Remove the stems from your kale leaves and discard. Finely chop kale leaves and cabbage. Remove skin from cooked beetroot and discard, then cut beets into bite sized cubes. Remove the stem of your broccoli and discard, and finely chop the florets. 
+    Add all your chopped veg to your quinoa bowl, and season with sea salt and pepper. Cut tofu into bite sized cubes and add. Sprinkle with pomegranate seeds and toss to combine.
+    
+    Make your dressing by adding lemon juice, diced garlic, tahini, tamari, apple cider vinegar, olive oil, cumin and ginger to a jar. Secure with lid and shake to combine. Season with salt and pepper to taste. 
+    Pour dressing over your quinoa bowl and mix to combine. Finely chop chives, cilantro and mint and sprinkle over the salad. Toss gently to combine. Season quinoa salad with more salt and pepper to taste and serve.
+    #wp #vegan  #veganism #veganfood #veganlife #veganlifestyle  #veganbodybuilding #vegansofig #veganrecipe #veganfoodporn  #veganfoodporn #organic #vegetarianfoodshare #vegetarian #health #healthyeating #healthylifestyle #healthysnacks #healthylife  #foodpics #foodgasm  #recipe #healthyrecipes #foodporn #foodie #plantbased #instagood #glutenfree #foodlover`
+    ,`Vibrant avocado vegan salad recipe 🥑🥑🥑😍😍😍😍.
+    .
+    .
+    Ingredients:
+    
+    1 1/2 cups uncooked #quinoa
+    
+    3 cups water
+    
+    2 #carrots
+    
+    1/4 white cabbage (approx. 1 cup)
+    
+    4 kale leaves
+    
+    1 broccoli head
+    
+    2 beetroot, cooked
+    
+    1 package (200g) firm tofu
+    
+    1/3 cup pomegranate seeds
+    
+    1 bunch chives
+    
+    1/2 cup packed mint leaves
+    
+    1/2 cup packed #cilantro
+    
+    1 avocado
+    
+    salt and pepper to taste
+    
+    For the dressing:
+    
+    the juice of 2 lemons
+    
+    3-4 garlic cloves, diced
+    
+    1/3 cup tahini
+    
+    1/4 cup tamari, or soy sauce
+    
+    2 tbsp apple cider vinegar,  or more lemon juice
+    
+    1 tbsp olive oil
+    
+    pinch #cumin
+    
+    pinch ground ginger 
+    Directions:
+    
+    Begin by cooking your quinoa. Place 1.5 cups quinoa and 3 cups water in a saucepan. Bring to a boil then reduce heat and let simmer for 12-15 minutes, or until cooked. Remove from stovetop and place in a large mixing bowl to cool.
+    
+    Peel and grate your carrots. Remove the stems from your kale leaves and discard. Finely chop kale leaves and cabbage. Remove skin from cooked beetroot and discard, then cut beets into bite sized cubes. Remove the stem of your broccoli and discard, and finely chop the florets. 
+    Add all your chopped veg to your quinoa bowl, and season with sea salt and pepper. Cut tofu into bite sized cubes and add. Sprinkle with pomegranate seeds and toss to combine.
+    
+    Make your dressing by adding lemon juice, diced garlic, tahini, tamari, apple cider vinegar, olive oil, cumin and ginger to a jar. Secure with lid and shake to combine. Season with salt and pepper to taste. 
+    Pour dressing over your quinoa bowl and mix to combine. Finely chop chives, cilantro and mint and sprinkle over the salad. Toss gently to combine. Season quinoa salad with more salt and pepper to taste and serve.
+    #vegan  #veganism #veganfood #veganlife #veganlifestyle  #veganbodybuilding #vegansofig #veganrecipe #veganfoodporn  #veganfoodporn #organic #vegetarianfoodshare #vegetarian #health #healthyeating #healthylifestyle #healthysnacks #healthylife  #foodpics #foodgasm  #recipe #healthyrecipes #foodporn #foodie #plantbased #instagood #glutenfree #foodlover`
+    ];
+}
